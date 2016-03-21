b8096c4e2dbb50aae6d77d418c6727ac9b0d9632
Karl Mae <karl.m2e@gmail.com>
2016-03-21 12:24:31 +0200

    File upload + login/register veateadetega

--- js/login.js
+++ js/login.js
@@ -7,6 +7,7 @@ $(function() {
     $('#login-form-link').click(function(e) {
 		$("#login-form").delay(100).fadeIn(100);
  		$("#register-form").fadeOut(100);
+        $('#alert').html("");
 		$('#register-form-link').removeClass('active');
 		$(this).addClass('active');
 		e.preventDefault();
@@ -14,6 +15,7 @@ $(function() {
 	$('#register-form-link').click(function(e) {
 		$("#register-form").delay(100).fadeIn(100);
  		$("#login-form").fadeOut(100);
+        $('#alert').html("");
 		$('#login-form-link').removeClass('active');
 		$(this).addClass('active');
 		e.preventDefault();
@@ -23,20 +25,36 @@ $(function() {
     $('#register-submit').click( function() {
 
        if($("#confirm-password").val() !== $("#register-password").val()) {
+           $('#alert').html("Paroolid peavad klappima!");
+           $('#alert').fadeIn().delay(2000);
+           return false;
+       } else if ($("#register-username").val() === "") {
+           $('#alert').html("Meil on vaja Su kasutajanime!");
+           $('#alert').fadeIn().delay(2000);
+           return false;
+       } else if ($("#fullname").val() === "") {
+           $('#alert').html("Meil on vaja Su täisnime!");
+           $('#alert').fadeIn().delay(2000);
+           return false;
+       } else if ($("#confirm-password").val() === "" || $("#register-password").val() === "") {
+           $('#alert').html("Parooliväli ei tohi jääda tühjaks!");
+           $('#alert').fadeIn().delay(2000);
            return false;
        } else {
            if($("#student-cb").is(':checked')) {
                var studentEntity = { 'username': $("#register-username").val(),
                                   'password': $("#confirm-password").val(),
-                                  'email': $("#email").val()};
+                                  'fullname': $("#fullname").val()};
                localStorage.setItem('studentEntity', JSON.stringify(studentEntity));
            } else {
                var teacherEntity = { 'username': $("#register-username").val(),
                                   'password': $("#confirm-password").val(),
-                                  'email': $("#email").val()};
+                                  'fullname': $("#fullname").val()};
                localStorage.setItem('teacherEntity', JSON.stringify(teacherEntity));
            }
-           alert('Registering successful.');
+           $('#alert').html("");
+           $('#alert-success').html("Registreerimine õnnestus!");
+           $('#alert-success').fadeIn().delay(2000);
            location.reload();
            return false;
        }
@@ -57,7 +75,8 @@ $(function() {
             window.location = "teacherindex.html";
             return false;
         } else {
-            alert('Wrong username or password');
+            $('#alert').html("Vale kasutajanimi või parool!");
+            $('#alert').fadeIn().delay(2000);
             return false;
         }
 
